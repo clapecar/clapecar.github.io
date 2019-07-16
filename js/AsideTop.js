b88cb0f7332a61558575484047f036a5212e0d7f
Clara <clarapenyalva@gmail.com>
2019-07-16 11:40:35 +0200

    borrada

--- js/AsideTop.js
+++
@@ -1,7 +0,0 @@
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > 130) {
-		document.getElementById('verticalGeneral').style.top = "0px";
-    } else {
-		document.getElementById('verticalGeneral').style.top = "200px";
-	}
-});
